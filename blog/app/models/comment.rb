161e38315bb6f2d41d7ed79c8f20189f2446b2be
GrilloPress <grillopress@gmail.com>
2014-10-22 21:39:54 +0000

    add further validations to comment and blog models to include minimums

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   
-  validates :author, :presence => true
+  validates :author, :body, :email :presence => true
   validates :body, length: { minimum: 5 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
-  validates :title, :presence => true
+  validates :title, :author, :body :presence => true
+  validates :body, length: { minimum: 15 }
+  validates :title, uniqueness: { case_sensitive: false }
   
   has_many :comments
 
