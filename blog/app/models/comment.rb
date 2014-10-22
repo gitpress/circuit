f2cc89cb94bc47b06508362054d1660f7d38a809
GrilloPress <grillopress@gmail.com>
2014-10-22 21:09:51 +0000

    add email to comments model and validation

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
   
   validates :author, :presence => true
   validates :body, length: { minimum: 5 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
